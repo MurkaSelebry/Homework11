0e1660ca403b6e2170c819648b81fc0cb1f8b004
selebry <murad1703@yandex.ru>
2022-11-22 21:34:00 +0300

    Добавьте файлы проекта.

---
+++ TipographWPF/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipographWPF
+{
+    class Person
+    {
+        public string SecondName { get { return "Ivanov"; } }
+        public override string ToString()
+        {
+            return "User Person";
+        }
+    }
+}
